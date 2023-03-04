1e50a1a9fcfd091ac087f6af2b3791328c963787
Serhii Pradid <115069705+Serhii-Pradid@users.noreply.github.com>
2023-03-04 16:38:36 +0200

    ЗД 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const markUp = createGalleryCard(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend' , markUp)
+
+function createGalleryCard(items) {
+return items.map(({preview, original, description}) => {
+
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`;
+}).join('')
+
+};
+
+//console.log(galleryItems)
+
+
+galleryContainer.addEventListener('click' , onGalleryContainerClick);
+
+function onGalleryContainerClick(evt) {
+evt.preventDefault();
+
+const currentImg = evt.target.dataset.source;
+
+if(!currentImg) {
+  return;
+}
+console.log(evt.target)
+
+const instance = basicLightbox.create(`
+<img src="${currentImg}"/>
+`)
+
+instance.show();
+
+}
+
